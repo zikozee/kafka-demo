4011772baa422ab0da00e60411a7e7a7bdd41106
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-10-02 17:13:14 +0100

    first kafka commit

---
+++ library-event-producer/src/test/java/io/zikozee/LibraryEventProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zikozee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
