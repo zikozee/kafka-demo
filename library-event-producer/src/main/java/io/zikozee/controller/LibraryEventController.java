fc9b562b5885b2635bf62dcc4fe611ccdf09d3fa
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-10-03 23:43:53 +0100

    showing asynchronous behaviour of kafkaTemplate closes #5

--- library-event-producer/src/main/java/io/zikozee/controller/LibraryEventController.java
+++ library-event-producer/src/main/java/io/zikozee/controller/LibraryEventController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import io.zikozee.domain.LibraryEvent;
 import io.zikozee.producer.LibraryEventProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @created: 03 October 2022
  */
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class LibraryEventController {
@@ -24,7 +26,9 @@ public class LibraryEventController {
     @PostMapping(path = "v1/libraryEvent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         // invoke Kafka producer
+        log.info(">>>>>>>> before sendLibrary event");
         producer.sendLibraryEvent(libraryEvent);
+        log.info(">>>>>>>> after sendLibrary event");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
